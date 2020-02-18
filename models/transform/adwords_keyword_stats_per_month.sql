eb36dbf3cce3a3cec2d91d0239e0fd56c062fa31
Yannis Roussos <iroussos@gitlab.com>
2020-02-18 11:56:57 +0200

    Add Keyword Stats per month

---
+++ models/transform/adwords_keyword_stats_per_month.sql
@@ -0,0 +1,73 @@
+with report as (
+
+     select *
+     from {{ref('adwords_keywords_report')}}
+
+)
+
+select
+
+  -- The Account, Campaign, Ad Group, Ad this report is for
+  account_id,
+  account_name,
+
+  campaign_id,
+  campaign_name,
+
+  ad_group_id,
+  ad_group_name,
+
+  keyword_id,
+  keyword,
+  keyword_status,
+
+  top_of_page_cpc,
+  first_page_cpc,
+
+  currency,
+
+  -- The start of the month this report is for
+  to_char(MIN(report_date), 'YYYY-MM') as month,
+  date_trunc('month', MIN(report_date))::date as month_start,
+
+  -- Generate a descriptive label: "2020-02 | Account | Campaign | Ad Group | Ad Type (Enabled)"
+  CONCAT
+  (
+    to_char(MIN(report_date), 'YYYY-MM'), ' | ',
+    account_name , ' | ' , campaign_name , ' | ' , ad_group_name , ' | ' ,
+    keyword , '(' , keyword_status , ')'
+  ) as label,
+
+  -- Aggregate Metrics for the report
+  {{ tap_adwords.core_ad_metrics_aggregates() }}
+
+from report
+
+group by
+  report_date_year,
+  report_date_month,
+  account_id,
+  account_name,
+  campaign_id,
+  campaign_name,
+  ad_group_id,
+  ad_group_name,
+  keyword_id,
+  keyword,
+  keyword_status,
+  top_of_page_cpc,
+  first_page_cpc,
+  currency
+
+-- Keyword Reports return one row per day even for keywords without any impressions
+-- Skip the noise from the long tail for campaigns with too many keywords defined
+having SUM(impressions) is not NULL
+
+order by
+  report_date_year,
+  report_date_month,
+  account_name,
+  campaign_name,
+  ad_group_name,
+  keyword,
+  keyword_status
