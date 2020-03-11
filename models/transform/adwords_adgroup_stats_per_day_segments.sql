8a977194849574b0708258f391d6fbcf5ed89346
Yannis Roussos <iroussos@gitlab.com>
2020-03-11 22:05:50 +0200

    Add models for AdGroup Stats per day/month and the additional segments (device, network)

---
+++ models/transform/adwords_adgroup_stats_per_day_segments.sql
@@ -0,0 +1,60 @@
+with report as (
+
+     select *
+     from {{ref('adwords_ad_report')}}
+
+)
+
+select
+
+  -- The Account, Campaign and Ad Group this report is for
+  account_id,
+  account_name,
+
+  campaign_id,
+  campaign_name,
+
+  ad_group_id,
+  ad_group_name,
+
+  currency,
+
+  -- The day (date) this report is for
+  report_date,
+
+  -- The Network and Device segments
+  network,
+  device,
+
+  -- Generate a descriptive label: "2020-01-16 | Search Network | Computers | Account | Campaign | Ad Group"
+  CONCAT
+  (
+    report_date, ' | ',
+    network, ' | ', device, ' | ',
+    account_name, ' | ', campaign_name, ' | ', ad_group_name
+  ) as label,
+
+  -- Metrics for the report
+  {{ tap_adwords.core_ad_metrics_aggregates() }}
+
+from report
+
+group by
+  report_date,
+  network,
+  device,
+  account_id,
+  account_name,
+  campaign_id,
+  campaign_name,
+  ad_group_id,
+  ad_group_name,
+  currency
+
+order by
+  report_date,
+  network,
+  device,
+  account_name,
+  campaign_name,
+  ad_group_name
---
+++ models/transform/adwords_adgroup_stats_per_month_segments.sql
@@ -0,0 +1,63 @@
+with report as (
+
+     select *
+     from {{ref('adwords_ad_report')}}
+
+)
+
+select
+
+  -- The Account, Campaign and Ad Group this report is for
+  account_id,
+  account_name,
+
+  campaign_id,
+  campaign_name,
+
+  ad_group_id,
+  ad_group_name,
+
+  currency,
+
+  -- The start of the month this report is for
+  to_char(MIN(report_date), 'YYYY-MM') as month,
+  date_trunc('month', MIN(report_date))::date as month_start,
+
+  -- The Network and Device segments
+  network,
+  device,
+
+  -- Generate a descriptive label: "2020-02 | Search Network | Computers | Account | Campaign | Ad Group"
+  CONCAT
+  (
+    to_char(MIN(report_date), 'YYYY-MM'), ' | ',
+    network, ' | ', device, ' | ',
+    account_name , ' | ' , campaign_name , ' | ' , ad_group_name
+  ) as label,
+
+  -- Aggregate Metrics for the report
+  {{ tap_adwords.core_ad_metrics_aggregates() }}
+
+from report
+
+group by
+  report_date_year,
+  report_date_month,
+  network,
+  device,
+  account_id,
+  account_name,
+  campaign_id,
+  campaign_name,
+  ad_group_id,
+  ad_group_name,
+  currency
+
+order by
+  report_date_year,
+  report_date_month,
+  network,
+  device,
+  account_name,
+  campaign_name,
+  ad_group_name
